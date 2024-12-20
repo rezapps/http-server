8e9c7a359c14000e9701733364d354487dd32d78
rezapps <122305716+rezapps@users.noreply.github.com>
2024-12-21 00:08:21 +0100

    added http respond

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,16 @@
 import * as net from "net";
 
-console.log("Logs from your program will appear here!");
+const PORT = 4221;
 
 const server = net.createServer((socket) => {
-  socket.on("close", () => {
+    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
     socket.end();
-  });
 });
 
-server.listen(4221, "localhost");
+server.listen(PORT, () => {
+    console.log(`Server is listening on port http://localhost:${PORT}`);
+});
+
+server.on('connection', (socket) => {
+    console.log('New client connected');
+});
