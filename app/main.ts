376b3c4360e5283f42c4513cc45ed115a626b4c3
rezapps <122305716+rezapps@users.noreply.github.com>
2024-12-21 04:00:17 +0100

    added response body and header

--- app/main.ts
+++ app/main.ts
@@ -3,15 +3,46 @@ import * as net from "net";
 const PORT = 4221;
 
 const server = net.createServer((socket) => {
-	// socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-
 	socket.on('data', (data) => {
 		const request = data.toString();
 		const path = request.split(' ')[1];
 
-		const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`;
+		let responseBody = '';
+		let contentType = '';
+		let contentLength = 0;
+		let responseHeaders = '';
 
-		socket.write(response);
+		if (path === '/') {
+			responseBody = `HTTP/1.1 200 OK\r\n\r\n`;
+			contentType = 'text/plain';
+			contentLength = responseBody.length;
+			responseHeaders = `HTTP/1.1 200 OK\r\n` +
+		`Content-Type: ${contentType}\r\n` +
+		`Content-Length: ${contentLength}\r\n` +
+		`\r\n`;
+		}
+		else if (path.startsWith('/echo/')) {
+			const message = path.slice(6);
+			responseBody = message;
+			contentType = 'text/plain';
+			contentLength = responseBody.length;
+			responseHeaders = `HTTP/1.1 200 OK\r\n` +
+		`Content-Type: ${contentType}\r\n` +
+		`Content-Length: ${contentLength}\r\n` +
+		`\r\n`;
+		}
+		else {
+			responseBody = '404 Not Found';
+			contentType = 'text/plain';
+			contentLength = responseBody.length;
+			responseHeaders = `HTTP/1.1 404 Not Found\r\n` +
+		`Content-Type: ${contentType}\r\n` +
+		`Content-Length: ${contentLength}\r\n` +
+		`\r\n`;
+		}
+
+		socket.write(responseHeaders);
+		socket.write(responseBody);
 		socket.end();
 	});
 
