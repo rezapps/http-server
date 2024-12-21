dbbeac286cbfd7838821d9a83bb7670b7edc2184
rezapps <122305716+rezapps@users.noreply.github.com>
2024-12-21 02:34:01 +0100

    added http request

--- app/main.ts
+++ app/main.ts
@@ -3,14 +3,20 @@ import * as net from "net";
 const PORT = 4221;
 
 const server = net.createServer((socket) => {
-    socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
-    socket.end();
+	// socket.write(Buffer.from(`HTTP/1.1 200 OK\r\n\r\n`));
+
+	socket.on('data', (data) => {
+		const request = data.toString();
+		const path = request.split(' ')[1];
+
+		const response = path === '/' ? `HTTP/1.1 200 OK\r\n\r\n` : `HTTP/1.1 404 Not Found\r\n\r\n`;
+
+		socket.write(response);
+		socket.end();
+	});
+
 });
 
-server.listen(PORT, () => {
-    console.log(`Server is listening on port http://localhost:${PORT}`);
-});
-
-server.on('connection', (socket) => {
-    console.log('New client connected');
+server.listen(PORT, 'localhost',() => {
+	console.log(`Server is listening on port http://localhost:${PORT}`);
 });
